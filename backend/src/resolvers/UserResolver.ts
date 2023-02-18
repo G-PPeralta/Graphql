20d992790a86f038772607de99b7c5f411f07e9f
G-PPeralta <prates_peralta@hotmail.com>
2023-02-18 11:58:35 -0300

    feat: implements query for listing users and mutation to create user

--- backend/src/resolvers/UserResolver.ts
+++ backend/src/resolvers/UserResolver.ts
@@ -1,9 +1,27 @@
-import { Query, Resolver } from "type-graphql";
+import { Arg, Mutation, Query, Resolver } from "type-graphql";
+import { User } from "../models/User";
+import crypto from "crypto";
+
+// query: buscar dados
+// mutation: criar, alterar ou deletar dados
 
 @Resolver()
 export class UserResolver {
-  @Query(() => String)
-  async hello() {
-    return "hello world";
+  private data: User[] = [];
+
+  @Query(() => [User])
+  async users() {
+    return this.data;
+  }
+
+  @Mutation(() => User)
+  async createUser(
+    @Arg("name") name: string,
+  ) {
+    const newUser = { id: crypto.randomUUID(), name}
+
+    this.data.push(newUser);
+
+    return newUser;
   }
 }
